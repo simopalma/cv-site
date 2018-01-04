dce5241ac2ec50cc552ceef1fefd9750f61d7f3d
Simone Palma <simone.palma@soprasteria.com>
2018-01-04 17:01:30 +0100

    fixed test

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import AppComponent from './components/AppComponent';
+import AppComponent from './components/appComponent';
 
 ReactDOM.render(
     <AppComponent />
